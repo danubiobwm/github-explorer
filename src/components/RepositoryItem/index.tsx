bbc8d8a0b79ca1b7efc394499a7bdf59a1a6b820
danubiobwm <danubio.bwm@gmail.com>
2022-03-13 17:41:02 -0300

    feat: :zap: Typescript

---
+++ src/components/RepositoryItem/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+interface IRepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  }
+}
+
+export function RepositoryItem(props: IRepositoryItemProps) {
+  
+  return (
+    <li>
+      <strong>{props.repository.name}</strong>
+      <p>{props.repository.description}</p>
+      <a href={props.repository.html_url}>acessar repository</a>
+    </li>
+  );
+}
